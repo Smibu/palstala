1984f7f96c6c9181b63bef342253e1dd4c403322
Mika Lehtinen <smibu90@gmail.com>
2021-07-02 23:46:33 +0300

    Fix GitHub login, add Prettier

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,16 +1,24 @@
-import NextAuth from "next-auth"
-import Providers from "next-auth/providers"
-import {PrismaAdapter} from "@next-auth/prisma-adapter"
-import {PrismaClient} from "@prisma/client"
+import NextAuth, { User as NextAuthUser } from "next-auth";
+import Providers from "next-auth/providers";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export default NextAuth({
   providers: [
     Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      profile(profile) {
+        return {
+          id: (profile.id as number).toString(),
+          name: (profile.name || profile.login) as string,
+          email: profile.email,
+          image: profile.avatar_url as string,
+        };
+      },
     }),
   ],
   adapter: PrismaAdapter(prisma),
-})
+});
