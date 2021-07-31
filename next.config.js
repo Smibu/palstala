d2ba9d6285152dad0896d2347c8e53dbef6b5b4f
Mika Lehtinen <smibu90@gmail.com>
2021-07-31 18:02:02 +0300

    Add a script for analyzing bundle size

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   distDir: process.env.BUILD_DIR || ".next",
-};
+});
