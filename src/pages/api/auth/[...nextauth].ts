0fc7c514ec20f29b56cc7e7cf892872b8f979273
Mika Lehtinen <smibu90@gmail.com>
2021-08-01 23:40:09 +0300

    Add GitLab as auth provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,22 +1,29 @@
-import NextAuth from "next-auth";
+import NextAuth, { Profile } from "next-auth";
 import Providers from "next-auth/providers";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import type { User } from "@prisma/client";
 import prisma from "../../../dbClient";
+
+function getProfileInfo(profile: Profile) {
+  return {
+    id: (profile.id as number).toString(),
+    name: (profile.name || profile.login) as string,
+    email: profile.email,
+    image: profile.avatar_url as string,
+  };
+}
 
 export default NextAuth({
   providers: [
     Providers.GitHub({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      profile(profile) {
-        return {
-          id: (profile.id as number).toString(),
-          name: (profile.name || profile.login) as string,
-          email: profile.email,
-          image: profile.avatar_url as string,
-        };
-      },
+      profile: getProfileInfo,
+    }),
+    Providers.GitLab({
+      clientId: process.env.GITLAB_CLIENT_ID,
+      clientSecret: process.env.GITLAB_CLIENT_SECRET,
+      profile: getProfileInfo,
     }),
   ],
   callbacks: {
